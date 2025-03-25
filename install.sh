689cf5a5c145209ed920a22534d94b27f1b31682
Daniel <liverant95@gmail.com>
2025-03-25 13:41:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/zsh
 
 DOTFILES_DIR="${HOME}/.dotfiles"
-ZSH_CUSTOM_DIR="${HOME}/.oh-my-zsh/custom"
 HOME_FILES_DIR="${DOTFILES_DIR}/home_files"
-
-ln -sf "${DOTFILES_DIR}/custom-zsh-config.zsh" "${ZSH_CUSTOM_DIR}/custom-zsh-config.zsh"
 
 for FILE in $HOME_FILES_DIR/*(DN); do
     FILE_NAME=$FILE:t
